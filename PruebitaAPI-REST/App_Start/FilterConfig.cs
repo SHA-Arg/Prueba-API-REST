f362d4e92b06e88346314270ed346502593c0f86
Seba HA <hereniu.amaral.sebastian@gmail.com>
2022-11-20 05:59:19 -0300

    Subiendo Prueba

---
+++ PruebitaAPI-REST/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PruebitaAPI_REST
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
